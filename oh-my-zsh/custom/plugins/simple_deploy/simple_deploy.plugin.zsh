8521e2e1c21dd552b1bb7ce1cfa4b4b51d3a345d
Bruce Li <libruce@amazon.com>
2016-10-29 23:54:16 +0200

    Remove unuse plugins

--- oh-my-zsh/custom/plugins/simple_deploy/simple_deploy.plugin.zsh
+++
@@ -1,5 +0,0 @@
-alias sd='simple_deploy'
-
-export SIMPLE_DEPLOY_SSH_USER=ec2-user
-
-compctl -k "(clone create deploy destroy environments events execute instances list outputs parameters protect resources status template update)" simple_deploy
--- oh-my-zsh/custom/plugins/tmux-session/tmux-session.plugin.zsh
+++
@@ -1,2 +0,0 @@
-[[ -f $ZSH_CUSTOM/plugins/tmux-session/tmux-session.plugin.zsh ]] && sudo ln -f -s $ZSH_CUSTOM/plugins/tmux-session/tmux-session /usr/local/bin/tmux-session
-[[ -f $ZSH/plugins/tmux-session/tmux-session.plugin.zsh ]] && sudo ln -f -s $ZSH/plugins/tmux-session/tmux-session /usr/local/bin/tmux-session
